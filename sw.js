962ff22f0587c78457dac629c2d3e606d57e94b4
itayura <itayuralevich@gmail.com>
2026-06-15 09:55:45 +0300

    fix: resolve Cyrillic stress mark rendering bug by using system font stack fallback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VocRussian Progressive Web App Service Worker
-const CACHE_NAME = "voc-russian-cache-v14";
+const CACHE_NAME = "voc-russian-cache-v15";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
