e57833c0643ecf851805ce8e1a157208f00d6043
itayura <itayuralevich@gmail.com>
2026-07-23 16:59:39 +0300

    Fix mobile aside navigation bar height to 70px to resolve Playwright layout test failure, bump sw cache to v24

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Privyetik Progressive Web App Service Worker
-const CACHE_NAME = "voc-russian-cache-v23";
+const CACHE_NAME = "voc-russian-cache-v24";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
