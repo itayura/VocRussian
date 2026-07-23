d870a75585bac0354d28191f38b4b5dc5eae672f
itayura <itayuralevich@gmail.com>
2026-07-23 17:54:23 +0300

    Fix mobile fixed bottom nav bar layout and bump cache v26

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Privyetik Progressive Web App Service Worker
-const CACHE_NAME = "voc-russian-cache-v26";
+const CACHE_NAME = "voc-russian-cache-v27";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
