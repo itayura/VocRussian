01a5ecd5e96b87239718480e693f795f535fe1ec
itayura <itayuralevich@gmail.com>
2026-07-23 17:15:39 +0300

    Override desktop min-height: 100vh on mobile main content area to prevent bottom nav bar from being pushed off-screen, bump sw cache to v25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Privyetik Progressive Web App Service Worker
-const CACHE_NAME = "voc-russian-cache-v24";
+const CACHE_NAME = "voc-russian-cache-v25";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
