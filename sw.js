ca111db583be37f2f3b4fbf2eb01f2b567c08fb0
itayura <itayuralevich@gmail.com>
2026-07-24 07:29:42 +0300

    High-contrast mobile bottom nav bar with icons and labels v29

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Privyetik Progressive Web App Service Worker
-const CACHE_NAME = "voc-russian-cache-v33";
+const CACHE_NAME = "voc-russian-cache-v34";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
