fe0b940c04a276db42a5616733b1d531908b0e0b
itayura <itayuralevich@gmail.com>
2026-07-31 22:23:40 +0300

    chore: bump app version to 1.0.7 (versionCode 10, cache-v35)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Privyetik Progressive Web App Service Worker
-const CACHE_NAME = "voc-russian-cache-v34";
+const CACHE_NAME = "voc-russian-cache-v35";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
