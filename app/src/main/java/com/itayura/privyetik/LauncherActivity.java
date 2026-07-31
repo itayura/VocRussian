eab943f90736117431c77159549595283e9e1cda
itayura <itayuralevich@gmail.com>
2026-07-31 14:41:57 +0300

    Implement androidx.activity EdgeToEdge.enable and remove deprecated status bar/nav bar metadata (v1.0.6, code 9)

--- app/src/main/java/com/itayura/privyetik/LauncherActivity.java
+++ app/src/main/java/com/itayura/privyetik/LauncherActivity.java
@@ -1,23 +1,16 @@
 package com.itayura.privyetik;
 
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Window;
+import androidx.activity.EdgeToEdge;
+import com.google.androidbrowserhelper.trusted.LauncherActivity;
 
 public class LauncherActivity extends com.google.androidbrowserhelper.trusted.LauncherActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
-        
-        // Edge-to-Edge display compliance for Android 15+ (API 35/36)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            Window window = getWindow();
-            if (window != null) {
-                window.setDecorFitsSystemWindows(false);
-            }
-        }
     }
 
     @Override
